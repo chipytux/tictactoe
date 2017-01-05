5b16c864fec350ab1c95ab2e4d9248598bc3abbb
chipytux <chipytux@users.noreply.github.com>
2017-01-05 11:19:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -129,13 +129,14 @@ function verificarVitoria(){
     }
 //-------------------------------------------------------------
     function fadeIt(v1,v2,v3){
-        for(var i=0;i<3;i++){
+        for(var i=0;i<8;i++){
             v1.fadeOut('fast');
             v2.fadeOut('fast');
             v3.fadeOut('fast');
             v1.fadeIn('fast');
             v2.fadeIn('fast');
             v3.fadeIn('fast');
+            jQuery.fx.off = true;
         }
     }
 
